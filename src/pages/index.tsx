9b6c3d9d060625d13162dd911dc6fbb349b5ddea
williamboomer87 <126490002+williamboomer87@users.noreply.github.com>
2024-12-17 09:57:42 +0530

    Update to add agent table

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,15 +22,16 @@ interface TableRowData {
     application_createtime: any;
     first_payment_date: any;
     payment_frequency: any;
-    data:any;
-    numberOfPages:any;
+    data: any;
+    numberOfPages: any;
 }
 
 interface Enrollment {
     monthToDateWonCount: any;
     monthToDateWonSum: any;
     yearlyData: any[];
-    monthlyData:any
+    monthlyData: any
+    agent: any
 }
 
 const Home: React.FC = () => {
@@ -38,31 +39,31 @@ const Home: React.FC = () => {
     const [limit, setLimit] = useState<any>(5);
     const [page, setPage] = useState<any>(1);
 
-    const { 
-        data: enrollmentData, 
-       // loading: loadingEnrollment, 
-       // error: enrollmentError 
+    const {
+        data: enrollmentData,
+        // loading: loadingEnrollment, 
+        // error: enrollmentError 
     } = useFetchData<Enrollment>({
-        url: "https://7fwwglseys3xlqk6hogiazspv40gzoug.lambda-url.us-east-1.on.aws/api/enrollments",
+        url: "http://localhost:5001/api/enrollments",
     });
 
-    const { 
-        data: tableDataResponse, 
-       // loading: loadingTableData, 
-       // error: tableDataError 
+    const {
+        data: tableDataResponse,
+        // loading: loadingTableData, 
+        // error: tableDataError 
     } = useFetchData<TableRowData>({
-        url: `https://7fwwglseys3xlqk6hogiazspv40gzoug.lambda-url.us-east-1.on.aws/api/enrollments/${limit}/${page}`,
+        url: `http://localhost:5001/api/enrollments/${limit}/${page}`,
     });
 
-    const { 
-        data: tableDataResponseMtd, 
-        loading: loadingTableDataMtd, 
-       // error: tableDataErrorMtd 
+    const {
+        data: tableDataResponseMtd,
+        loading: loadingTableDataMtd,
+        // error: tableDataErrorMtd 
     } = useFetchData<TableRowData>({
-        url: `https://7fwwglseys3xlqk6hogiazspv40gzoug.lambda-url.us-east-1.on.aws/api/enrollments/mtd/${limit}/${page}`,
+        url: `http://localhost:5001/api/enrollments/mtd/${limit}/${page}`,
     });
 
-    console.log("tableDatatableDatatableDatatableData",tableDataResponse)
+    console.log("tableDatatableDatatableDatatableData", tableDataResponse)
     const handleChange = (event: React.SyntheticEvent, newValue: string) => {
         setValue(newValue);
     };
@@ -86,49 +87,75 @@ const Home: React.FC = () => {
                             <Tab label="Individual enrollment" value="2" />
                         </TabList>
                     </Box>
-                    <TabPanel value="1" sx={{backgroundColor:'#e5e7eb'}}>
-                        <Box  sx={{  borderRadius: '10px' }}>
-                        <Typography variant="h5" sx={{ fontWeight: 600 }}>YTD Enrollments</Typography>
+                    <TabPanel value="1" sx={{ backgroundColor: '#e5e7eb' }}>
+                        <Box sx={{ borderRadius: '10px' }}>
+                            <Typography variant="h5" sx={{ fontWeight: 600 }}>YTD Enrollments</Typography>
 
-                            <Grid container>
-
+                            <Grid container spacing={2}>
                                 <Grid item xs={12} md={6} sx={{ padding: '20px' }}>
-                                 
-                                        <MainCard title="YTD Enrollments" count={String(enrollmentData?.monthToDateWonCount || 0)} />
-                                  
+                                    <MainCard title="YTD Enrollments" count={String(enrollmentData?.monthToDateWonCount || 0)} />
                                 </Grid>
                                 <Grid item xs={12} md={6} sx={{ padding: '20px' }}>
-                                <MainCard title="Enrolled Debt" count={(enrollmentData?.monthToDateWonSum || 0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} />
-
+                                    <MainCard title="Enrolled Debt" count={(enrollmentData?.monthToDateWonSum || 0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} />
                                 </Grid>
-                                <TableContainer sx={{ margin: '20px' }}>
-                                        <Table sx={{ minWidth: 500, backgroundColor: '#fff', borderRadius: '10px' }} aria-label="custom pagination table">
-                                            <TableHead>
-                                                <TableRow>
-                                                    <TableCell align="left" sx={{ fontSize: 18, color: 'gray', fontWeight: 700 }}>Enrolled Date</TableCell>
-                                                    <TableCell align="left" sx={{ fontSize: 18, color: 'gray', fontWeight: 700 }}>Total Enrollments</TableCell>
-                                                    <TableCell align="left" sx={{ fontSize: 18, color: 'gray', fontWeight: 700 }}>Debt Enrollments</TableCell>
-                                                </TableRow>
-                                            </TableHead>
-                                            <TableBody>
-                                                {enrollmentData?.yearlyData?.map((row, key) => (
-                                                    <TableRow key={key} >
-                                                        <TableCell align="left">{row?.enrolled_date}</TableCell>
-                                                        <TableCell align="left">{row?.total_enrollments}</TableCell>
-                                                        <TableCell align="left">{row?.debt_enrolled}</TableCell>
+
+                                {/* Two tables horizontally */}
+                                <Grid container spacing={2}>
+                                    <Grid item xs={12} md={6}>
+                                        <TableContainer sx={{ margin: '20px', backgroundColor: '#fff', borderRadius: '10px' }}>
+                                            <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
+                                                <TableHead>
+                                                    <TableRow>
+                                                        <TableCell align="left" sx={{ fontSize: 18, color: 'gray', fontWeight: 700 }}>Enrolled Date</TableCell>
+                                                        <TableCell align="left" sx={{ fontSize: 18, color: 'gray', fontWeight: 700 }}>Total Enrollments</TableCell>
+                                                        <TableCell align="left" sx={{ fontSize: 18, color: 'gray', fontWeight: 700 }}>Debt Enrollments</TableCell>
                                                     </TableRow>
-                                                ))}
-                                            </TableBody>
-                                        </Table>
-                                    </TableContainer>
+                                                </TableHead>
+                                                <TableBody>
+                                                    {enrollmentData?.yearlyData?.map((row, key) => (
+                                                        <TableRow key={key}>
+                                                            <TableCell align="left">{row?.enrolled_date}</TableCell>
+                                                            <TableCell align="left">{row?.total_enrollments}</TableCell>
+                                                            <TableCell align="left">{row?.debt_enrolled}</TableCell>
+                                                        </TableRow>
+                                                    ))}
+                                                </TableBody>
+                                            </Table>
+                                        </TableContainer>
+                                    </Grid>
+
+                                    <Grid item xs={12} md={5}>
+                                        <TableContainer sx={{ marginRight: '50px',marginTop:'20px', marginLeft:'20px', backgroundColor: '#fff', borderRadius: '10px' }}>
+                                            <Table sx={{  }} aria-label="second table">
+                                                <TableHead>
+                                                    <TableRow>
+                                                        <TableCell align="left" sx={{ fontSize: 18, color: 'gray', fontWeight: 700 }}>Agent name</TableCell>
+                                                        <TableCell align="left" sx={{ fontSize: 18, color: 'gray', fontWeight: 700 }}>Total Enrollments</TableCell>
+                                                        <TableCell align="left" sx={{ fontSize: 18, color: 'gray', fontWeight: 700 }}>Debt Enrolled</TableCell>
+                                                    </TableRow>
+                                                </TableHead>
+                                                <TableBody>
+                                                    {/* Replace with your second table's data */}
+                                                    {enrollmentData?.agent?.map((row: any, key: any) => (
+                                                        <TableRow key={key}>
+                                                            <TableCell align="left">{row?.firstname} {row?.lastname}</TableCell>
+                                                            <TableCell align="left">{row?.assigned_application_count}</TableCell>
+                                                            <TableCell align="left">$ { row?.loan_amount}</TableCell>
+                                                        </TableRow>
+                                                    ))}
+                                                </TableBody>
+                                            </Table>
+                                        </TableContainer>
+                                    </Grid>
+                                </Grid>
                             </Grid>
                         </Box>
-                        
                     </TabPanel>
-                    <TabPanel value="2" sx={{backgroundColor:'#e5e7eb'}}>
-                   
+
+                    <TabPanel value="2" sx={{ backgroundColor: '#e5e7eb' }}>
+
                         <TableContainer sx={{ marginTop: '50px' }}>
-                            <Table sx={{ minWidth: 500, backgroundColor: '#fff', borderRadius: '10px' }} aria-label="custom pagination table">
+                            <Table sx={{  backgroundColor: '#fff', borderRadius: '10px' }} aria-label="custom pagination table">
                                 <TableHead>
                                     <TableRow>
                                         <TableCell align="left" sx={{ fontSize: 16, color: 'gray', fontWeight: 700 }}>ID</TableCell>
@@ -143,7 +170,7 @@ const Home: React.FC = () => {
                                     {loadingTableDataMtd ? (
                                         <CircularProgress sx={{ width: 30, height: 30, marginLeft: 10 }} />
                                     ) : (
-                                        tableDataResponseMtd?.data?.map((row:any, key:any) => (
+                                        tableDataResponseMtd?.data?.map((row: any, key: any) => (
                                             <TableRow key={key} >
                                                 <TableCell align="left">{row?.reservationcode}</TableCell>
                                                 <TableCell align="left">{row?.contact_firstname} {row?.contact_lastname}</TableCell>
@@ -168,7 +195,7 @@ const Home: React.FC = () => {
                             />
                         </TableContainer>
                     </TabPanel>
-                    
+
                 </TabContext>
             </Box>
         </Box>
